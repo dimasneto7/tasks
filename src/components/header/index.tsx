0b67a4df38adf06f82bf55ff2252acfa88fa6cfd
dimasneto7 <dimas_fcn@outlook.com>
2024-11-15 16:39:27 -0300

    dashboard

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,10 +1,12 @@
 import styles from './styles.module.css'
+import { useSession, signIn, signOut } from 'next-auth/react'
 import Link from 'next/link'
 import Image from 'next/image'
 
 import Logo from '../../../public/assets/logo.svg'
 
 export function Header() {
+  const { data: session, status } = useSession()
   return (
     <header className={styles.header}>
       <section className={styles.content}>
@@ -13,11 +15,23 @@ export function Header() {
             <Image src={Logo} className={styles.logoImg} alt="Logo" priority />
             <h1 className={styles.logo}>Tasks</h1>
           </Link>
-          <Link href="/dashboard" className={styles.dashlink}>
-            Meu Painel
-          </Link>
+          {session?.user && (
+            <Link href="/dashboard" className={styles.dashlink}>
+              Meu Painel
+            </Link>
+          )}
         </nav>
-        <button className={styles.loginButton}>Acessar</button>
+        {status === 'loading' ? (
+          <></>
+        ) : session ? (
+          <button className={styles.loginButton} onClick={() => signOut()}>
+            Olá {session?.user?.name}
+          </button>
+        ) : (
+          <button className={styles.loginButton} onClick={() => signIn()}>
+            Acessar
+          </button>
+        )}
       </section>
     </header>
   )
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,11 @@
+import { GetServerSideProps } from 'next'
 import Head from 'next/head'
 import styles from './styles.module.css'
+
+import { getSession } from 'next-auth/react'
+import { Textarea } from '@/components/textarea'
+import { FiShare2 } from 'react-icons/fi'
+import { FaTrash } from 'react-icons/fa'
 
 export default function Dashboard() {
   return (
@@ -7,7 +13,61 @@ export default function Dashboard() {
       <Head>
         <title>Painel de Controle de Tasks</title>
       </Head>
-      <h1>Página Painel</h1>
+      <main className={styles.main}>
+        <section className={styles.content}>
+          <div className={styles.contentForm}>
+            <h1 className={styles.title}>Qual sua tarefa?</h1>
+            <form>
+              <Textarea placeholder="Digite qual sua tarefa..." />
+              <div className={styles.checkboxArea}>
+                <input type="checkbox" className={styles.checkbox} />
+                <label>Deixar tarefa pública?</label>
+              </div>
+              <button className={styles.button} type="submit">
+                Registrar
+              </button>
+            </form>
+          </div>
+        </section>
+
+        <section className={styles.taskContainer}>
+          <h1>Minhas Tarefas</h1>
+          <article className={styles.task}>
+            <div className={styles.tagContainer}>
+              <label className={styles.tag}>PÚBLICO</label>
+              <button className={styles.shareButton}>
+                <FiShare2 size={22} color="#3183ff" />
+              </button>
+            </div>
+            <div className={styles.taskContent}>
+              <p>Minha primeira tarefa de exemplo sho demais</p>
+              <button className={styles.trashButton}>
+                <FaTrash size={24} color="#ea3140" />
+              </button>
+            </div>
+          </article>
+        </section>
+      </main>
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  console.log('Buscando pelo ServerSide')
+  const session = await getSession({ req })
+  console.log(session)
+
+  if (!session?.user) {
+    // se não tem usuário vamos redirecionar para home
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    }
+  }
+
+  return {
+    props: {},
+  }
 }
